770613e681e883a1f328290bd1b09c2e1dc6644b
Amam Mustofa <mustofa@binary.com>
2019-04-03 03:07:00 +0800

    add api for plant

--- src/routes/plant.js
+++ src/routes/plant.js
@@ -3,6 +3,85 @@ let express = require('express')
 let router = express.Router()
 let multer = require('multer')
 let path = require('path')
+
+// SECTION 1 REST API
+
+//post
+router.post('/plant', (req, res) => {
+    upload(req, res, (err) => {
+        const rootDir = req.protocol + '://' + req.get('host');
+        if (err) {
+            res.render('index', {
+                msg: err
+            })
+        } else {
+            if (req.file === undefined) {
+                res.render('index', {
+                    msg: 'Error: no file selected'
+                })
+            } else {
+                if (!Object.keys(req.body).length === 0) res.status(400).send('Request body is missing')
+                const plantModel = new PlantModel({
+                    name: req.body.name,
+                    imagePath: `${rootDir}/images/${req.file.filename}`,
+                    price: req.body.price,
+                    description: req.body.description
+                })
+                plantModel.save()
+                    .then(doc => {
+                        if (!doc || doc.length === 0) res.status(500).send(doc)
+
+                        res.status(201).send(doc)
+                    })
+                    .catch(err => res.status(500).json(err))
+                res.render('add', {
+                    msg: 'File uploaded'
+                })
+            }
+        }
+    })
+})
+
+//get
+router.get('/plant', (req, res) => {
+    PlantModel.find().then(doc => res.json(doc))
+        .catch(err => res.status(500).json(err))
+})
+
+// get specific
+router.get('/plant/:id', (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send('Missing URL parameter id')
+    }
+
+    PlantModel.findOne({
+            _id: req.params.id,
+        }).then(doc => res.json(doc))
+        .catch(err => res.status(500).json(err))
+})
+//put
+//TODO: UPDATE API
+router.put('/plant/:id', (req, res) => {
+    if (!req.params.id) res.status(400).send('Missing URL parameter id')
+
+    PlantModel.findOneAndUpdate({
+        _id: req.params.id,
+    }, req.body, {
+        new: true,
+    }).then(doc => res.json(doc))
+      .catch(err => res.status(500).json(err))
+})
+
+//delete
+router.delete('/plant/:id', (req, res) => {
+    if (!req.params.id) res.status(400).send('Missing URL parameter id')
+
+    PlantModel.findOneAndRemove({
+            _id: req.params.id,
+        }).then(doc => res.json(doc))
+        .catch(err => res.status(500).json(err))
+})
+// SECTION 2 ADMIN ROUTING AND FUNCTION
 // set storage multer
 const storage = multer.diskStorage({
     destination: './public/images/',
@@ -38,42 +117,7 @@ const checkFileType = (file, callback) => {
     }
 }
 
-router.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        const rootDir = req.protocol + '://' + req.get('host');
-        if (err) {
-            res.render('index', {
-                msg: err
-            })
-        } else {
-            if (req.file === undefined) {
-                res.render('index', {
-                    msg: 'Error: no file selected'
-                })
-            } else {
-                if (!Object.keys(req.body).length === 0) res.status(400).send('Request body is missing')
-                const plantModel = new PlantModel({
-                    name: req.body.name,
-                    imagePath: `${rootDir}/images/${req.file.filename}`,
-                    price: req.body.price,
-                    description: req.body.description
-                })
-                plantModel.save()
-                    .then(doc => {
-                        if (!doc || doc.length === 0) res.status(500).send(doc)
-
-                        res.status(201).send(doc)
-                    })
-                    .catch(err => res.status(500).json(err))
-                res.render('index', {
-                    msg: 'File uploaded',
-                    file: `images/${req.file.filename}`
-                })
-            }
-        }
-    })
-})
-
+// this is admin page route
 router.get('/view', (req, res) => {
     PlantModel.find().then(doc => {
             res.render('view', {
